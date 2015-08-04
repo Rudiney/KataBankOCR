fdbd971c28198bc7c9f6c566e9038a495bddf284
Rudiney Altair Franceschi <rudi.atp@gmail.com>
2015-08-03 22:51:48 -0300

    acctoun has numbers as array and a method #illegible

--- lib/account.rb
+++ lib/account.rb
@@ -1,17 +1,21 @@
 class Account
-  attr_accessor :number
+  attr_accessor :numbers
 
   def valid?
     valid_checksum?
   end
 
+  def illebile?
+    numbers.include?(nil)
+  end
+
   private
 
   def valid_checksum?
-    reverse_digits = number.to_s.split('').reverse
+    reverse_numbers = numbers.reverse
     checksum = 0
 
-    reverse_digits.each_with_index do |digit, index|
+    reverse_numbers.each_with_index do |digit, index|
       checksum += (index+1)*digit.to_i
     end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,24 +2,41 @@ require 'spec_helper'
 
 RSpec.describe Account do
 
-  it { expect(subject).to respond_to(:number) }
+  it { expect(subject).to respond_to(:numbers) }
 
   describe '#valid?' do
     it { expect(subject).to respond_to(:valid?) }
 
-    it 'should validate the checksum of the number' do
-      subject.number = 345882865
+    it 'should validate the checksum of the numbers' do
+      subject.numbers = [3,4,5,8,8,2,8,6,5]
       expect(subject).to be_valid
     end
 
     it ' 457508000 is valid' do
-      subject.number = 457508000
+      subject.numbers = [4,5,7,5,0,8,0,0,0]
       expect(subject).to be_valid
     end
 
     it ' 664371495 is not valid' do
-      subject.number = 664371495
+      subject.numbers = [6,6,4,3,7,1,4,9,5]
       expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#illegible' do
+    it 'when there is nill numberss, is illegible' do
+      subject.numbers = [1,2,3,nil,6,7]
+      expect(subject).to be_illebile
+    end
+
+    it 'empty is not illebile?' do
+      subject.numbers = []
+      expect(subject).to_not be_illebile
+    end
+
+    it 'filled is not illebile?' do
+      subject.numbers = [1,2,3]
+      expect(subject).to_not be_illebile
     end
   end
 end
