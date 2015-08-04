dfdc212431e06380cb75688a9ef0017b60a13b91
Rudiney Altair Franceschi <rudi.atp@gmail.com>
2015-08-03 23:15:11 -0300

    account to_s

--- lib/account.rb
+++ lib/account.rb
@@ -1,12 +1,26 @@
 class Account
   attr_accessor :numbers
 
+  def initialize(numbers=nil)
+    self.numbers = numbers
+  end
+
   def valid?
     valid_checksum?
   end
 
-  def illebile?
+  def illegible?
     numbers.include?(nil)
+  end
+
+  def to_s
+    return numbers_string + ' ILL' if illegible?
+    return numbers_string + ' ERR' if !valid?
+    numbers_string
+  end
+
+  def numbers_string
+    numbers.map { |n| n.nil? ? '?' : n.to_s }.join
   end
 
   private
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -26,17 +26,75 @@ RSpec.describe Account do
   describe '#illegible' do
     it 'when there is nill numberss, is illegible' do
       subject.numbers = [1,2,3,nil,6,7]
-      expect(subject).to be_illebile
+      expect(subject).to be_illegible
     end
 
-    it 'empty is not illebile?' do
+    it 'empty is not illegible?' do
       subject.numbers = []
-      expect(subject).to_not be_illebile
+      expect(subject).to_not be_illegible
     end
 
-    it 'filled is not illebile?' do
+    it 'filled is not illegible?' do
       subject.numbers = [1,2,3]
-      expect(subject).to_not be_illebile
+      expect(subject).to_not be_illegible
+    end
+
+    it 'with 0s is not illegible?' do
+      subject.numbers = [0,0,0]
+      expect(subject).to_not be_illegible
+    end
+  end
+
+  describe '#numbers_string' do
+    it 'translate nil to ?' do
+      subject.numbers = [1,nil,nil,4]
+      expect(subject.numbers_string).to eql '1??4'
+    end
+  end
+
+  describe '#to_s' do
+    it 'should join numbers' do
+      subject.numbers = [0,0,0,0,0,0,0,0,0]
+      expect(subject).to be_valid
+      expect(subject).to be_valid
+      expect(subject.to_s).to eql '000000000'
+
+      subject.numbers = [1,2,3,4,5,6,7,8,9]
+      expect(subject).to be_valid
+      expect(subject.to_s).to eql '123456789'
+    end
+
+    describe 'when invalid' do
+      it 'should add ERR' do
+        expect(subject).to receive(:valid?).and_return(false)
+        subject.numbers = [1,2,3,4,5,6,7,8,9]
+        expect(subject.to_s).to eql '123456789 ERR'
+      end
+    end
+
+    describe 'when illegible' do
+      it 'should add ILL' do
+        expect(subject).to receive(:illegible?).and_return(true)
+        subject.numbers = [1,2,3,4,5,6,7,8,9]
+        expect(subject.to_s).to eql '123456789 ILL'
+      end
+    end
+
+    describe 'test case' do
+      it '457508000 is ok' do
+        subject.numbers = [4,5,7,5,0,8,0,0,0]
+        expect(subject.to_s).to eql '457508000'
+      end
+
+      it '664371495 has ERR' do
+        subject.numbers = [6,6,4,3,7,1,4,9,5]
+        expect(subject.to_s).to eql '664371495 ERR'
+      end
+
+      it '86110??36 is ILL' do
+        subject.numbers = [8,6,1,1,0,nil,nil,3,6]
+        expect(subject.to_s).to eql '86110??36 ILL'
+      end
     end
   end
 end
